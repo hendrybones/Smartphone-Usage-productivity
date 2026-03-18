99f54d46fdd9cfef18d06489a21a3345b40b66b8
Hendrybones <hendrymwamburi56@gmail.com>
2026-03-18 18:14:06 +0300

    adding run pipeline script

--- src/data/run_pipeline.py
+++ src/data/run_pipeline.py
@@ -1,12 +1,19 @@
-from src.data.ingestation import ingest_data, load_config   
+from src.data.ingestion import ingest_data, load_config
+from src.data.cleaning import clean_data
 from src.data.validation import run_data_validation
 from src.data.preprocessors import preprocess_data
 
-if __name__ == "__main__":
+def main():
     config = load_config()
     
-    df = ingest_data(config)
+    ingest_data(config)
+    clean_data(config)
     
+    # Optional: stop if validation fails
+    df = clean_data(config)  # if your clean_data returns df
     if run_data_validation(df, config):
         preprocess_data(config)
-    print
+
+
+if __name__ == "__main__":
+    main()
